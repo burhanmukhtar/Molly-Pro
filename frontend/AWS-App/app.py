0bd0cf9ec012215acc24a07e95d8c785b0dd38a1
Burhan mukhtar <62393908+burhanmukhtar@users.noreply.github.com>
2025-04-14 01:24:04 +0530

    Update app.py

--- frontend/AWS-App/app.py
+++ frontend/AWS-App/app.py
@@ -51,8 +51,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Setup AWS connection
-AWS_ACCESS_KEY = "AKIAWBC22NAVY7DQBICN"  
-AWS_SECRET_KEY = "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"
+AWS_ACCESS_KEY = "acesskey"  
+AWS_SECRET_KEY = "secretkey"
 
 # Define AWS regions to use
 AWS_REGIONS = [
